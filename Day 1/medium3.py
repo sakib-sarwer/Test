619dbfd7a70f1b3815931e1790ba97e3704d4ce3
sakib-sarwer <sakib.tcil@gmail.com>
2025-10-11 09:22:18 +0300

    medium3 done

---
+++ Day 1/medium3.py
@@ -0,0 +1,7 @@
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Pleae enter the second number: "))
+number3 = float(input("Pleae enter the third number: "))
+
+average = ((number1+number2+number3)/3)
+
+print("The average of 3 numbers is: ", average)
