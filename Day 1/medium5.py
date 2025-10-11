3c3e8c8e04f0e816db01d10466328bde87835f2b
sakib-sarwer <sakib.tcil@gmail.com>
2025-10-11 09:38:53 +0300

    medium5 done

---
+++ Day 1/medium5.py
@@ -0,0 +1,9 @@
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+
+number1 = number1+number2
+number2 = number1-number2
+number1 = number1-number2
+
+print("The value of first number is changed to: ", number1)
+print("The value of second number is changed to: ", number2)
