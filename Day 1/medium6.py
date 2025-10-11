9fede1e7cfbc0324ab7790586ab8ee6b93647d6f
sakib-sarwer <sakib.tcil@gmail.com>
2025-10-11 09:45:15 +0300

    medium6 done

---
+++ Day 1/medium6.py
@@ -0,0 +1,7 @@
+P = float(input("enter the principal amount: "))
+R = float(input("enter the rate of interest: "))
+T = float(input("enter the time in years: "))
+
+SI = (P*R*T)/100
+
+print("The simple interest is: ", SI)
