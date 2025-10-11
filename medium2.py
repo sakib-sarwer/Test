ee622835028f84b6b3e19cf3e27a868c840e4dc7
sakib-sarwer <sakib.tcil@gmail.com>
2025-10-11 09:13:56 +0300

    medium2 done

---
+++ medium2.py
@@ -0,0 +1,6 @@
+dividend = float(input("please enter the dividend: "))
+divisor = float(input("please enter the divisor: "))
+quotient = int((dividend/divisor))
+remainder = dividend - (divisor*quotient)
+
+print("the remainder is: ", remainder)
