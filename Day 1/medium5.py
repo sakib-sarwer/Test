c8e7fd2725308d85f5425fdcfb3a0ca2983b7868
sakib-sarwer <sakib.tcil@gmail.com>
2025-10-12 15:55:28 +0300

    made edits

--- Day 1/medium5.py
+++ Day 1/medium5.py
@@ -1,3 +1,7 @@
+# Swap two numbers without a third variable (using arithmetic)
+
+# Solution
+
 number1 = float(input("Please enter the first number: "))
 number2 = float(input("Please enter the second number: "))
 
@@ -5,5 +9,5 @@ number1 = number1+number2
 number2 = number1-number2
 number1 = number1-number2
 
-print("The value of first number is changed to: ", number1)
-print("The value of second number is changed to: ", number2)
+print(f"The value of first number is changed to:  {number1}")
+print(f"The value of second number is changed to:  {number2}")
--- Day 1/medium6.py
+++ Day 1/medium6.py
@@ -1,7 +1,11 @@
+# Calculate simple interest: SI = (P * R * T) / 100
+
+# Solution
+
 P = float(input("enter the principal amount: "))
 R = float(input("enter the rate of interest: "))
 T = float(input("enter the time in years: "))
 
 SI = (P*R*T)/100
 
-print("The simple interest is: ", SI)
+print(f"The simple interest is:  {SI}")
