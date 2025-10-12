cc86cdd06511fcac111bc5868e0fb3b876c6eff9
sakib-sarwer <sakib.tcil@gmail.com>
2025-10-12 15:39:13 +0300

    made edits

--- Day 1/easy7.py
+++ Day 1/easy7.py
@@ -1,3 +1,7 @@
+# Calculate the area of a square
+
+# Solution
+
 side = float(input("Please enter the value of side of a square: "))
 area = side*side
 print ("The area of the square is = "  , area)
