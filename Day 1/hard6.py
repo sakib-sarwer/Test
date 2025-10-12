67a7e5ef5d12ba1126fd89401e9badc2b258567a
sakib-sarwer <sakib.tcil@gmail.com>
2025-10-12 17:54:44 +0300

    problem solved

---
+++ Day 1/hard6.py
@@ -0,0 +1,9 @@
+# Convert the ASCII value to a character
+
+# Solution
+
+ascii_value = int(input("Enter an ASCII value: "))
+
+character = chr(ascii_value)
+
+print("The character for ASCII value", ascii_value, "is", character)
